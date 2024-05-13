03f48048b4123462bb91db523e43cd4e42d5a417
Gizmo <me@aika.dev>
2024-05-13 18:28:34 +0800

    fix: prevent log content from wrapping when exceeding window width
    Closes #1

--- app/pages/logs.tsx
+++ app/pages/logs.tsx
@@ -1,13 +1,53 @@
 import { defineComponent } from "vue"
-import { NVirtualList } from "naive-ui"
+import { NTag, NText, NVirtualList, NEllipsis } from "naive-ui"
+import { formatISO9075 } from "date-fns"
 import PageLayout from "~/layout/PageLayout.tsx"
-import LogItem from "~/components/LogItem.tsx"
 import { type LogEntry, utilGetLogEntries } from "~/helper/index.ts"
+
+type NTagType = "default" | "info" | "error" | "success" | "primary" | "warning"
+
+function levelTag(level: string) {
+  let tagType: NTagType = "default"
+  switch (level) {
+    case "Info":
+      tagType = "success"
+      break
+    case "Warn":
+      tagType = "warning"
+      break
+    case "Error":
+      tagType = "error"
+      break
+  }
+  const levelStr = level
+  return (
+    <NTag
+      type={tagType}
+      bordered={false}
+      size="small"
+      class="w-full flex justify-center"
+    >
+      {levelStr}
+    </NTag>
+  )
+}
+
+function logItem(item: LogEntry) {
+  return (
+    <div class="flex gap-2 min-h-7 w-full">
+      <NText class="flex w-4rem">{levelTag(item.level)}</NText>
+      <NText class="flex">{formatISO9075(new Date(item.time))}</NText>
+      <NEllipsis class="flex w-[calc(100%-1rem-4rem-10rem)]">
+        <NText>{item.message}</NText>
+      </NEllipsis>
+    </div>
+  )
+}
 
 export default defineComponent({
   setup() {
     let logs = $ref<LogEntry[]>([])
-    utilGetLogEntries().then((list: LogEntry[]) => (logs = list.reverse()))
+    utilGetLogEntries().then((list: LogEntry[]) => (logs = list))
 
     return () => (
       <PageLayout title="Logs">
@@ -17,7 +57,7 @@ export default defineComponent({
           items={logs}
           class="h-[calc(100vh-5rem)]"
         >
-          {({ item }: { item: LogEntry }) => <LogItem item={item} />}
+          {({ item }: { item: LogEntry }) => logItem(item)}
         </NVirtualList>
       </PageLayout>
     )
