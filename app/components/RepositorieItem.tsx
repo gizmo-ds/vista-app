c90fd4b2e02f66cb056c04cb2d434aecf3daced5
Gizmo <me@aika.dev>
2024-05-13 18:28:44 +0800

    chore: remove unused components

--- app/components/RepositorieItem.tsx
+++
@@ -1,9 +0,0 @@
-import { defineComponent, type PropType } from "vue"
-import { type TauriUserRepository } from "~/helper/index.ts"
-
-export default defineComponent({
-  props: { repo: Object as PropType<TauriUserRepository> },
-  setup(props) {
-    return () => <div>RepositorieItem: {props.repo?.display_name}</div>
-  }
-})
