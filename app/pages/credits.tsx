8c13d1159364a33f74213bed3bd02e4dc4d5a9a4
Gizmo <me@aika.dev>
2024-05-14 23:50:20 +0800

    chore: update license information text on Credits page

--- app/pages/credits.tsx
+++ app/pages/credits.tsx
@@ -1,7 +1,8 @@
 import { defineComponent } from "vue"
+import { NAlert } from "naive-ui"
 import { type License as ILicense, getLicenses } from "~/helper/index.ts"
 import PageLayout from "~/layout/PageLayout.tsx"
-import LicenseItem from "@/app/components/LicenseItem"
+import LicenseItem from "~/components/LicenseItem"
 
 export default defineComponent({
   setup() {
@@ -10,9 +11,17 @@ export default defineComponent({
 
     return () => (
       <PageLayout title="Credits" hasBack={true}>
-        <p class="mt-0">
-          This project is made possible by the following open-source projects:
-        </p>
+        <div class="mt-0 mb-2">
+          <p>
+            This project is made possible by the following open-source projects.
+            Each is listed with its respective license information.
+          </p>
+          <NAlert type="warning">
+            While we provide complete license texts where available, some
+            projects may lack full license documentation. For these, a link to
+            the project's homepage is provided for further licensing details.
+          </NAlert>
+        </div>
         {/* perf sucks, but nobody cares 😔 */}
         <div class="flex flex-col gap-2">
           {[...licenses].map((l, i) => (
